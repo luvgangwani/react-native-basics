32152fb1e0a27cf943b89385fd6dba9b338d95e9
Luv Gangwani <luv.gangwani@gmail.com>
2021-02-21 00:23:01 +1100

    Lesson 3 - Views, Text and Styles

--- App.js
+++ App.js
@@ -5,7 +5,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Header View</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Et velit adipisicing consectetur Lorem amet exercitation sunt in qui ex incididunt.</Text>
+        <Text>Nisi officia tempor minim deserunt nulla reprehenderit laborum laborum do excepteur cillum enim.</Text>
+        <Text style={styles.boldText}>Styles are not inherited from View to Text but are inherited from <Text>Text to Text</Text></Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +24,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: '#23E76A',
+    padding: 20,
+    color: '#000000',
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: '#56EEF4',
+    padding: 40,
+    color: '#000000',
   },
 });
