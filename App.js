059b5d648a1cf3b8230a6b50ec044e473106779b
Luv Gangwani <luv.gangwani@gmail.com>
2021-02-21 01:02:51 +1100

    Lesson 4 - Using states

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Alpha');
+  const [person, setPerson] = useState({ name: 'Gamma', age: 40 });
+
+  const handleUpdateStatusClick = () => {
+    setName('Beta');
+    setPerson({
+      name: 'Delta',
+      age: 45,
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Header View</Text>
-      </View>
-      <View style={styles.body}>
-        <Text>Et velit adipisicing consectetur Lorem amet exercitation sunt in qui ex incididunt.</Text>
-        <Text>Nisi officia tempor minim deserunt nulla reprehenderit laborum laborum do excepteur cillum enim.</Text>
-        <Text style={styles.boldText}>Styles are not inherited from View to Text but are inherited from <Text>Text to Text</Text></Text>
+      <Text>My name is {name}.</Text>
+      <Text>His name is {person.name} and his age is {person.age}.</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Update State" onPress={handleUpdateStatusClick}/>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -25,17 +33,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor: '#23E76A',
-    padding: 20,
-    color: '#000000',
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  body: {
-    backgroundColor: '#56EEF4',
-    padding: 40,
-    color: '#000000',
-  },
+  buttonContainer: {
+    marginTop: 20,
+  }
 });
