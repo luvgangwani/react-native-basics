37cec2ab0c38b35caa72d552ca1bf24b11570ceb
Luv Gangwani <luv.gangwani@gmail.com>
2021-02-21 23:14:15 +1100

    Lesson 19 - Todo App - Part 2 - Styling list items and deleting on press

--- App.js
+++ App.js
@@ -2,10 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import Header from './components/header';
-
-// In flat lists items not all the items are loaded at once. They are loaded as we scroll through the list (lazy loading)
-// Specify a differently named key property (default is "key", in this case "id") using the keyExtractor prop
-// We can adjust the items in grids using the numColumns prop
+import TodoItem from './components/todoItem';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -13,6 +10,10 @@ export default function App() {
     {text: 'create an app', key: '2'},
     {text: 'play on the switch', key: '3'},
   ])
+
+  const pressHandler = (itemKey) => {
+    setTodos((prevTodos) => prevTodos.filter(({ key }) => key != itemKey));
+  }
 
   return (
     <View style={styles.container}>
@@ -22,7 +23,7 @@ export default function App() {
         <View style={styles.list}>
           <FlatList 
             data={todos}
-            renderItem={({ item }) => <Text>{item.text}</Text>}
+            renderItem={({ item }) => <TodoItem item={ item } onPressHandler={pressHandler} />}
           />
         </View>
       </View>
---
+++ components/todoItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+export default function TodoItem({ item, onPressHandler }) {
+    return (
+        <TouchableOpacity onPress={() => onPressHandler(item.key)}>
+            <Text style={styles.item}>{item.text}</Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    item: {
+        padding: 25,
+        margin: 10,
+        backgroundColor: '#023e8a',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderRadius: 10,
+        color: '#ffffff',
+    }
+})
