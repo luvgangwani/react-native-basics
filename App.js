b1b443778f6e97bbce0542db933b79d88d1bbabf
Luv Gangwani <luv.gangwani@gmail.com>
2021-02-22 20:44:10 +1100

    Lesson 14 - Flexbox Refer: https://www.youtube.com/watch?v=R2eqAgR_KlU

--- App.js
+++ App.js
@@ -61,9 +61,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   content: {
+    flex: 1,
     padding: 40,
   },
   list: {
-    marginTop: 20
+    flex: 1,
+    marginTop: 20,
   }
 });
